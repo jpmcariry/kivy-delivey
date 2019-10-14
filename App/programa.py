c8379b902b2956ebaa90bf1b04c95a4c792306e4
jpmcariry <39745896+jpmcariry@users.noreply.github.com>
2019-10-14 20:49:17 -0300

    Add files via upload

--- App/programa.py
+++ App/programa.py
@@ -1,10 +1,32 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import FadeTransition
+
+class Tela1(Screen, object):
+    def funcao(self):
+        print("dentro da funcao")
+    def verificar(self):
+        if(self.ids.login.text=="test" and self.ids.senha.text=="test"):
+            self.manager.current="Tela2"
+        else:
+            print("login e senha incorretos")
+    def mudar_btn(self):
+        self.ids.btn.back_color=""
+        print("deve ter muado")
 
 
+class Tela2(Screen, object):
+    pass
 
-arquivo_kv = Builder.load_file("kivy_lang.kv")
+class Window(ScreenManager, object):
+    pass
+
+arquivo_kv = Builder.load_file("programa.kv")
+screenmanager = Window()
+screenmanager.add_widget(Tela1())
 
 class ProgramaApp(App):
     def build(self):
